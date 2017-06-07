d015017886aba50f29568360c43422c433515924
Marron Akanishi <marron.general@gmail.com>
2017-06-07 18:21:27 +0900

    update

--- main.py
+++ main.py
@@ -1,119 +1,230 @@
-# Flask などの必要なライブラリをインポートする
+# ajaxはPOST、それ以外はGET
 import os
 import glob
-from base64 import decodestring
+import json
 import DBreader as db
-import tweepy
+import tweepy as tp
 import flask
+from functools import wraps
+import zipfile
+import urllib
+import threading
+# TL回収用
+import adminTL
+import gettweet
 
 # 自身の名称を app という名前でインスタンス化する
 app = flask.Flask(__name__)
-# session用のキー
-app.secret_key = "konokacyankawaii"
+setting = json.load(open("setting.json"))
 
-# DB一覧
-filelist = []
+# 認証後に使用可能
+def tp_api():
+    auth = tp.OAuthHandler(setting['twitter_API']['CK'], setting['twitter_API']['CS'], setting['twitter_API']['Callback_URL'])
+    auth.set_access_token(flask.session['key'],flask.session['secret'])
+    return tp.API(auth)
 
-# Twitter認証用キー
-CONSUMER_KEY = "Z0ZmTzB3cmFQdmV0Ym1KbFdqWWcwWHh1Rg=="
-CONSUMER_SECRET = "U3RIcEJYaFhFZFhUV09EaUxCVHd0TkFmT3c5TGtKdGJjZzJJOHZYaTVoNkhJeDlPWm4="
-CALLBACK_URL = "http://localhost:5000/authed" #実行環境によって変更すること
+def admin_check():
+    return flask.session['name'] == setting['AdminID']
+
+def login_check(func):
+    @wraps(func)
+    def checker(*args, **kwargs):
+        # きちんと認証していればセッション情報がある
+        try:
+            if flask.session['userID'] is None:
+                return flask.redirect(flask.url_for('index'))
+        except:
+            return flask.redirect(flask.url_for('index'))
+        return func(*args, **kwargs)
+    return checker
 
 # ここからウェブアプリケーション用のルーティングを記述
-# index にアクセスしたときの処理
+# トップページ
 @app.route('/')
 def index():
-    # index.html をレンダリングする
-    global filelist
-    filelist = sorted([path.split(os.sep)[1].split('.')[0] for path in glob.glob("collect/*.db")])
-    return flask.render_template('index.html', dblist=filelist, select=filelist[-1])
+    key = flask.request.cookies.get('key')
+    secret = flask.request.cookies.get('secret')
+    if key is None or secret is None:
+        return flask.render_template('index.html')
+    else:
+        flask.session['key'] = key
+        flask.session['secret'] = secret
+        return flask.redirect(flask.url_for('twitter_authed', cookie=True))
 
-# aboutページ
-@app.route('/about')
-def about():
-    return flask.render_template('about.html')
-
-# twitter認証ページ
+# twitter認証
 @app.route('/twitter_auth', methods=['GET'])
 def twitter_oauth():
-    # tweepy でアプリのOAuth認証を行う
-    key = decodestring(CONSUMER_KEY.encode("utf8")).decode("ascii")
-    secret = decodestring(CONSUMER_SECRET.encode("utf8")).decode("ascii")
-    auth = tweepy.OAuthHandler(key, secret, CALLBACK_URL)
+    # cookieチェック
+    key = flask.request.cookies.get('key')
+    secret = flask.request.cookies.get('secret')
+    if key is None or secret is None:
+        # tweepy でアプリのOAuth認証を行う
+        auth_temp = tp.OAuthHandler(setting['twitter_API']['CK'], setting['twitter_API']['CS'], setting['twitter_API']['Callback_URL'])
+        # 連携アプリ認証用の URL を取得
+        redirect_url = auth_temp.get_authorization_url()
+        # 認証後に必要な request_token を session に保存
+        flask.session['request_token'] = auth_temp.request_token
+        # リダイレクト
+        return flask.redirect(redirect_url)
+    else:
+        flask.session['key'] = key
+        flask.session['secret'] = secret
+        return flask.redirect(flask.url_for('twitter_authed', cookie=True))
 
-    # 連携アプリ認証用の URL を取得
-    redirect_url = auth.get_authorization_url()
-    # 認証後に必要な request_token を session に保存
-    # 辞書型で保管される
-    flask.session['request_token'] = auth.request_token
-
-    # リダイレクト
-    return flask.redirect(redirect_url)
-
-# twitter認証完了ページ
+# twitter認証完了
 @app.route('/authed', methods=['GET'])
 def twitter_authed():
-    return flask.redirect(flask.url_for('index'))
+    # 認証情報取得
+    if flask.request.args.get('cookie') != "True":
+        auth_temp = tp.OAuthHandler(setting['twitter_API']['CK'], setting['twitter_API']['CS'], setting['twitter_API']['Callback_URL'])
+        auth_temp.request_token = flask.session['request_token']
+        auth_temp.get_access_token(flask.request.args.get('oauth_verifier'))
+        flask.session['key'] = auth_temp.access_token
+        flask.session['secret'] = auth_temp.access_token_secret
+        flask.session['request_token'] = None
+    # 認証ユーザー取得
+    flask.session['name'] = tp_api().me().screen_name
+    flask.session['userID'] = tp_api().me().id_str
+    response = flask.make_response(flask.redirect(flask.url_for('user_page')))
+    response.set_cookie('key', flask.session['key'])
+    response.set_cookie('secret', flask.session['secret'])
+    return response
 
-# /list にアクセスしたときの処理
-@app.route('/list', methods=['GET'])
+# ログアウトボタン
+@app.route('/logout')
+def logout():
+    response = flask.make_response(flask.redirect(flask.url_for('index')))
+    response.set_cookie('key', '', expires=0)
+    response.set_cookie('secret', '', expires=0)
+    flask.session.clear()
+    return response
+
+# こっから下は認証が必要
+# ユーザーメニュー
+@app.route('/menu')
+@login_check
+def user_page():
+    filelist = []
+    if admin_check() or setting['AdminShow']:
+        filelist = sorted([path.split(os.sep)[1].split('.')[0] for path in glob.glob("DB/admin/*.db")])
+    return flask.render_template('menu.html', admin=admin_check(), showadminTL=setting['AdminShow'], dblist=filelist, select=filelist[-1], count=setting["MaxCount"])
+
+# ログページ
+@app.route('/admin/logs')
+@login_check
+def log_page():
+    if admin_check():
+        loglist = sorted([path.split(os.sep)[1].split('.')[0] for path in glob.glob("DB/log/*.log")])
+    else:
+        flask.redirect(flask.url_for('user_page'))
+    return flask.render_template('log.html', filelist=loglist)
+
+@app.route('/getlog', methods=['POST'])
+@login_check
+def get_log():
+    if admin_check():
+        filename = flask.request.form['date']
+        log = open("DB/log/{}.log".format(filename))
+        text = log.read()
+        log.close()
+    else:
+        flask.redirect(flask.url_for('user_page'))
+    if text == "":
+        text = "まだログは記録されていません"
+    return text
+
+# 共通ページ
+# 画像リストビュー生成
+@app.route('/makelist', methods=['POST'])
+@login_check
+def make_list():
+    mode = flask.request.form['mode']
+    dbname = flask.session['userID']
+    if mode == "timeline":
+        gettweet.gethomeTL(tp_api(),setting["MaxCount"])
+    elif mode == "user":
+        userid = flask.request.form['userid']
+        gettweet.getuserTL(tp_api(),userid,setting["MaxCount"])
+    elif mode == "list":
+        url = flask.request.form['url']
+        gettweet.getlist(tp_api(),url,setting["MaxCount"])
+    elif mode == "tag":
+        hashtag = flask.request.form['hashtag']
+        gettweet.gethashtag(tp_api(),hashtag,setting["MaxCount"])
+    elif mode == "admin":
+        if admin_check() == False and setting['AdminShow'] == False:
+                return "/error.html"
+        dbname = flask.request.form['date']
+    return "/view?mode={}&dbname={}".format(mode,dbname)
+
+@app.route('/view', methods=['GET'])
+@login_check
 def image_list():
-    global filelist
-    if flask.request.method == 'GET':
-        # リクエストフォーム取得して
-        date = flask.request.args.get('date')
-        # 画像一覧生成
-        try:
-            images,count = db.get_list("collect/" + date + ".db")
-        except:
-            return flask.render_template('error.html')
-        # index.html をレンダリングする
-        return flask.render_template('list.html',
-                dblist=filelist, select=date, filelist=images, count=count)
-    else:
-        # エラーなどでリダイレクトしたい場合はこんな感じで
-        return flask.redirect(flask.url_for('index'))
+    mode = flask.request.args.get('mode')
+    dbname = flask.session['userID']
+    try:
+        if mode == "admin":
+            if admin_check() == False and setting['AdminShow'] == False:
+                return flask.render_template('error.html')
+            dbname = flask.request.args.get('dbname')
+            images,count = db.get_list("DB/admin/" + dbname + ".db", "list")
+        else:
+            images,count = db.get_list("DB/user/" + dbname + ".db", mode)
+    except:
+        return flask.render_template('error.html')
+    return flask.render_template('view.html', filelist=images, count=count, mode=mode, dbname=dbname)
 
-# /search にアクセスしたとき
-@app.route('/search', methods=['GET'])
-def image_search():
-    global filelist
-    if flask.request.method == 'GET':
-        # リクエストフォーム取得して
-        date = flask.request.args.get('date')
-        userid = flask.request.args.get('userid')
-        # 画像一覧生成
-        try:
-            images,count = db.search_db(userid, "collect/" + date + ".db")
-        except:
-            return flask.render_template('error.html')
-        # index.html をレンダリングする
-        return flask.render_template('list.html',
-                dblist=filelist, select=date, filelist=images, count=count)
-    else:
-        # エラーなどでリダイレクトしたい場合はこんな感じで
-        return flask.redirect(flask.url_for('index'))
-
-# /list/detail にアクセスしたときの処理
-@app.route('/list/detail', methods=['GET'])
+# 画像詳細
+@app.route('/detail', methods=['GET'])
+@login_check
 def image_detail():
-    if flask.request.method == 'GET':
-        # リクエストフォーム取得して
-        image_id = flask.request.args.get('id')
-        date = flask.request.args.get('date')
-        # 画像情報
-        try:
-            detail,html,count,idinfo = db.get_detail(int(image_id), "collect/"+date+".db")
-        except:
-            return flask.render_template('error.html')
-        if html == "error":
-            html = "<p>ツイートが存在しません</p>"
-        # index.html をレンダリングする
-        return flask.render_template('detail.html', 
-            data=detail, html=html, date=date, max=count, idcount=idinfo)
-    else:
-        # エラーなどでリダイレクトしたい場合はこんな感じで
-        return flask.redirect(flask.url_for('index'))
+    mode = flask.request.args.get('mode')
+    image_id = flask.request.args.get('id')
+    dbname = flask.session['userID']
+    try:
+        if mode == "admin":
+            if admin_check() == False and setting['AdminShow'] == False:
+                return flask.render_template('error.html')
+            dbname = flask.request.args.get('dbname')
+            detail,html,idinfo = db.get_detail(int(image_id), "DB/admin/"+dbname+".db", "list")
+        else:
+            detail,html,idinfo = db.get_detail(int(image_id), "DB/user/"+dbname+".db", mode)
+    except:
+        return flask.render_template('error.html')
+    # index.html をレンダリングする
+    return flask.render_template('detail.html', data=detail, html=html, idcount=idinfo)
+
+# 一括ダウンロード
+@app.route('/download', methods=['POST'])
+@login_check
+def download():
+    #画像一覧取得
+    mode = flask.request.form['mode']
+    dbname = flask.session['userID']
+    try:
+        if mode == "admin":
+            if admin_check() == False and setting['AdminShow'] == False:
+                return "/error.html"
+            dbname = flask.request.form['dbname']
+            images,count = db.get_list("DB/admin/" + dbname + ".db", "list")
+        else:
+            images,count = db.get_list("DB/user/" + dbname + ".db", mode)
+    except:
+        return "/error.html"
+    zipdata = zipfile.ZipFile('static/zip/{}.zip'.format(flask.session['userID']),'w',zipfile.ZIP_DEFLATED)
+    for i,image in enumerate(images):
+        root, ext = os.path.splitext(image['image'])
+        temp_file = urllib.request.urlopen(image['image']+":orig").read()
+        zipdata.writestr(str(i).zfill(5)+ext,temp_file)
+        temp_file = None
+    zipdata.close()
+    return '/static/zip/{}.zip'.format(flask.session['userID'])
+
+# 認証不要ページ
+# このページについて
+@app.route('/about')
+def about():
+    return flask.render_template('about.html', count=setting["MaxCount"])
 
 # 404エラー
 @app.errorhandler(404)
@@ -121,5 +232,9 @@ def page_not_found(error):
     return flask.render_template('error.html')
 
 if __name__ == '__main__':
-    app.debug = True # デバッグモード有効化
+    t1 = adminTL.TLThread()
+    t1.setDaemon(True)
+    t1.start()
+    app.secret_key = setting['SecretKey']
+    app.debug = setting['Debug'] # デバッグモード
     app.run(host='0.0.0.0') # どこからでもアクセス可能に
