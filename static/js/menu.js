262e94efe9b38410316af78fe0a484c73baec376
Marron Akanishi <marron.general@gmail.com>
2017-10-07 02:06:08 +0900

    表示周り修正

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,5 +1,16 @@
+var getDevice = (function () {
+    var ua = navigator.userAgent;
+    if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0) {
+        return 'sp';
+    } else if (ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0) {
+        return 'tab';
+    } else {
+        return 'other';
+    }
+})();
+
 window.onload = function(){
-    if (window.Notification && Notification.permission === 'default') {
+    if (window.Notification && Notification.permission === 'default' && getDevice === 'other') {
         // 許可が取れていない場合はNotificationの許可を取る
         Notification.requestPermission()
     }
@@ -20,7 +31,7 @@ jQuery(function($){
             },
             success: function (resultdata) {
                 if(resultdata.indexOf('/view') != -1){
-                    if (window.Notification  && Notification.permission === 'granted') {
+                    if (window.Notification && Notification.permission === 'granted' && getDevice === 'other') {
                         var n = new Notification("取得が完了しました");
                         n.onshow = function(){location.href = resultdata}
                     }else{
